6ed41d386b08a4b8a2cc161a1cd62278a1bf2d36
ikenshu <undefined.hlo.o@gmail.com>
2016-09-12 10:36:34 -0400

    Agregando vista de detalle

--- spells/urls.py
+++ spells/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from .views import SpellListView, SpellSearchView
+from .views import SpellDetailView, SpellListView, SpellSearchView
 
 app_name = 'Spell'
 urlpatterns = [
     url(r'^$', SpellListView.as_view(), name='spell_list'),
     url(r'^buscar/$', SpellSearchView.as_view() ,name='spell_search'),
+    url(r'^buscar/(?P<slug>[-\w]+)/$', SpellDetailView.as_view(), name='spell_detail'),
 ]
--- spells/views.py
+++ spells/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.template.context import RequestContext
 from django.http import JsonResponse
 
-from django.views.generic import ListView, View
+from django.views.generic import DetailView, ListView, View
 
 from .models import Spell, Group
 
@@ -13,8 +13,6 @@ class SpellListView(ListView):
 	template_name = 'spells/spell_list.html'
 	context_object_name = 'spells'
 
-
-
 class SpellSearchView(View):
 	model = Spell
 	def get(self, request, *args, **kwargs):
@@ -22,3 +20,8 @@ class SpellSearchView(View):
 			spells = self.model.objects.filter(name__istartswith=request.GET['name']).values('id', 'name', 'slug')
 			return JsonResponse(list(spells), safe=False)
 		return JsonResponse("Solo se permiten consultas mediante AJAX", safe=False)
+
+class SpellDetailView(DetailView):
+    model = Spell
+    template_name = 'spells/spell_detail.html'
+    slug_field = 'name'
