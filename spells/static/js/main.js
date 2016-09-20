ef1d795dffadd6afb8c120e7ae8407c648c34f6d
ikenshu <undefined.hlo.o@gmail.com>
2016-09-20 15:54:44 -0400

    Agregando javascript para un ligero menu desplegable

---
+++ spells/static/js/main.js
@@ -0,0 +1,16 @@
+var libro = document.getElementById('libro')
+var libros = document.getElementById('libros')
+
+libro.addEventListener('click', function(e){
+    e.preventDefault()
+    if (libros.style.display == 'none')
+    {
+        libros.style.display = 'block'
+    }
+    else
+    {
+        libros.style.display = 'none'
+    }
+})
+
+console.log(libros)
