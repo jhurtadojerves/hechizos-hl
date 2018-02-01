4f5b08023d747256d17d6b4495bbc064ad18ea3b
Julio Hurtado <juliens@outlook.com>
2018-02-01 11:44:50 -0500

    Update wsgi.py

--- hechizos/wsgi.py
+++ hechizos/wsgi.py
@@ -5,3 +5,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hechizos.settings")
 
 application = get_wsgi_application()
+
+from whitenoise.django import DjangoWhiteNoise
+
+application = DjangoWhiteNoise(application)
