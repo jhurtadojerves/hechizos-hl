409131be497bcd7a8cd41b14fdaf7564120fcf56
Julio Hurtado <juliens@outlook.com>
2018-02-26 09:25:21 -0500

    split setting and wsgi files

---
+++ hechizos/heroku_settings.py
@@ -0,0 +1,18 @@
+import dj_database_url
+import os
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+DATABASES = {'default': dj_database_url.config()}
