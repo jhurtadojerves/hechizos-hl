bdb720dad360cd0c0aaf720f11d20cf68d350ba3
Julio Hurtado <juliens@outlook.com>
2018-02-27 19:42:13 -0500

    Fix settings.production file

--- hechizos/settings/production.py
+++ hechizos/settings/production.py
@@ -6,4 +6,11 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['http://hechizos.herokuapp.com/']
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 DATABASES = {'default': dj_database_url.config()}
