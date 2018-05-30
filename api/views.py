ae89411b14c5b6c9c89cb963ae4eac9adb6b785a
Julio Hurtadoe <juliohurtado.backend@gmail.com>
2018-05-29 20:45:58 -0500

    List Spells by range API

--- api/views.py
+++ api/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 from .serializers import SpellSerializer
 
-from spells.models import Spell
+from spells.models import Spell, Range
+
 
 
 class SpellAPIList(ListAPIView):
@@ -15,3 +16,11 @@ class SpellAPIDetail(RetrieveAPIView):
     queryset = Spell.objects.all()
     serializer_class = SpellSerializer
     lookup_field = 'slug'
+
+
+class SpellRangeAPIList(ListAPIView):
+    serializer_class = SpellSerializer
+
+    def get_queryset(self):
+        range = get_object_or_404(Range, slug=self.kwargs['slug'])
+        return Spell.objects.filter(range=range, battles=True)
