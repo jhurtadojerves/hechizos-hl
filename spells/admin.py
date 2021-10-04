e916acca0a097f42be889fbfa2dcf8d2273ce81b
Julio Hurtado (Juliens) <juliens@outlook.com>
2021-10-04 13:12:50 -0500

    :sparkles: add spell filtering in django admin

--- spells/admin.py
+++ spells/admin.py
@@ -5,22 +5,52 @@ from .resources import GroupResource, SpellResource, RangeResource, ImportExport
 
 @admin.register(Group)
 class GroupAdmin(ImportExportMixin, admin.ModelAdmin):
-    list_display = ['id', 'name', 'slug', ]
-    list_editable = ['name', ]
+    list_display = [
+        "id",
+        "name",
+        "slug",
+    ]
+    list_editable = [
+        "name",
+    ]
     class_source = GroupResource
 
 
 @admin.register(Spell)
 class SpellAdmin(ImportExportMixin, admin.ModelAdmin):
-    list_display = ['id', 'name', 'type', 'method', 'object', 'slug', ]
-    list_editable = ['name', 'type', 'method', 'object', ]
-    list_filter = ['type', ]
-    search_fields = ['name', ]
+    list_display = [
+        "id",
+        "name",
+        "type",
+        "method",
+        "object",
+        "slug",
+    ]
+    list_editable = [
+        "name",
+        "type",
+        "method",
+        "object",
+    ]
+    list_filter = [
+        "type",
+        "range__group",
+        "range",
+    ]
+    search_fields = [
+        "name",
+    ]
     list_per_page = 25
     class_source = SpellResource
 
 
 @admin.register(Range)
 class RangeAdmin(ImportExportMixin, admin.ModelAdmin):
-    list_display = ['id', 'name', 'group', 'slug', ]
+    list_display = [
+        "id",
+        "name",
+        "group",
+        "group",
+        "slug",
+    ]
     class_source = RangeResource
